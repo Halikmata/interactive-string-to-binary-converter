e5369e8cdbcd29542c62378aa71b19a76662e9f7
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2023-10-05 15:43:21 +0800

    Add project files.

---
+++ interactive string to binary converter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace interactive_string_to_binary_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.lbl_input = new System.Windows.Forms.Label();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.lbl_output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(146, 69);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(810, 174);
+            this.txt_input.TabIndex = 0;
+            this.txt_input.TextChanged += new System.EventHandler(this.txt_input_TextChanged);
+            // 
+            // lbl_input
+            // 
+            this.lbl_input.AutoSize = true;
+            this.lbl_input.Font = new System.Drawing.Font("Mongolian Baiti", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_input.Location = new System.Drawing.Point(405, 23);
+            this.lbl_input.Name = "lbl_input";
+            this.lbl_input.Size = new System.Drawing.Size(302, 43);
+            this.lbl_input.TabIndex = 1;
+            this.lbl_input.Text = "INPUT STRING";
+            this.lbl_input.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(146, 342);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.Size = new System.Drawing.Size(810, 174);
+            this.txt_output.TabIndex = 2;
+            // 
+            // lbl_output
+            // 
+            this.lbl_output.AutoSize = true;
+            this.lbl_output.Font = new System.Drawing.Font("Mongolian Baiti", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_output.Location = new System.Drawing.Point(315, 281);
+            this.lbl_output.Name = "lbl_output";
+            this.lbl_output.Size = new System.Drawing.Size(480, 43);
+            this.lbl_output.TabIndex = 3;
+            this.lbl_output.Text = "BINARY TRANSLATION";
+            this.lbl_output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 576);
+            this.Controls.Add(this.lbl_output);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.lbl_input);
+            this.Controls.Add(this.txt_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label lbl_input;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Label lbl_output;
+    }
+}
+
---
+++ interactive string to binary converter/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interactive_string_to_binary_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string input_text = txt_input.Text, output_text = "";
+
+        }
+
+        private void txt_input_TextChanged(object sender, EventArgs e)
+        {
+            int length = txt_input.TextLength;
+
+            //txt_output.Text = length.ToString();
+
+            for (int i = 0; i < length; i++)
+            {
+                char character = txt_input.Text[i];
+                string asciival = "";
+
+                switch (character)
+                {
+                    case 'A':
+                        asciival = "00110000";
+                        break;
+
+                    case 'B':
+                        asciival = "00110000";
+                        break;
+
+                    case 'C':
+                        asciival = "00110000";
+                        break;
+
+                    case 'D':
+                        asciival = "00110000";
+                        break;
+
+                    case 'E':
+                        asciival = "00110000";
+                        break;
+
+                    case 'F':
+                        asciival = "00110000";
+                        break;
+
+                    case 'G':
+                        asciival = "00110000";
+                        break;
+
+                    case 'H':
+                        asciival = "00110000";
+                        break;
+
+                    case 'I':
+                        asciival = "00110000";
+                        break;
+
+                    case 'J':
+                        asciival = "00110000";
+                        break;
+
+                    case 'K':
+                        asciival = "00110000";
+                        break;
+
+                    case 'L':
+                        asciival = "00110000";
+                        break;
+
+                    case 'M':
+                        asciival = "00110000";
+                        break;
+
+                    case 'N':
+                        asciival = "00110000";
+                        break;
+
+                    case 'O':
+                        asciival = "00110000";
+                        break;
+
+                    case 'P':
+                        asciival = "00110000";
+                        break;
+
+                    case 'Q':
+                        asciival = "00110000";
+                        break;
+
+                    case 'R':
+                        asciival = "00110000";
+                        break;
+
+                    case 'S':
+                        asciival = "00110000";
+                        break;
+
+                    case 'T':
+                        asciival = "00110000";
+                        break;
+
+                    case 'U':
+                        asciival = "00110000";
+                        break;
+
+                    case 'V':
+                        asciival = "00110000";
+                        break;
+
+                    case 'W':
+                        asciival = "00110000";
+                        break;
+
+                    case 'X':
+                        asciival = "00110000";
+                        break;
+
+                    case 'Y':
+                        asciival = "00110000";
+                        break;
+
+                    case 'Z':
+                        asciival = "00110000";
+                        break;
+
+
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
